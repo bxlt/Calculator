490557a8630bb5188a0513ed17fb09e3cffe1a2b
bxlt <xuelitan123@gmail.com>
2024-04-21 23:28:35 -0400

    create add/subtract/multiply/div func

---
+++ main.js
@@ -0,0 +1,43 @@
+let first = 0;
+let second = 0;
+let currOpt = 0;
+
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2")  + "e-2");
+}
+
+function add(a,b){
+    return roundToTwo(a+b);
+}
+
+function subtract(a,b){
+    return roundToTwo(a-b);
+}
+
+function multiply(a,b){
+    return roundToTwo(a*b);
+}
+
+function divide(a,b){
+    res = 0;
+    if(b==0){
+        res = NaN;
+    }else if(a!=0){
+        res = roundToTwo(a/b)
+    }
+    return res;
+}
+
+function operate(num1, num2,opt){
+    let res;
+    if(opt==1){
+        res = add(num1,num2)
+    }else if (opt==2){
+        res = subtract(num1, num2)
+    }else if(opt==3){
+        res = multiply(num1, num2)
+    }else if(opt==4){
+        res = divide(num1, num2)
+    }
+    return res;
+}
